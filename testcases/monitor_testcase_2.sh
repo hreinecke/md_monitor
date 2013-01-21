d9fe720dec3f68dd3c2d3189c6cefee015704f75
Hannes Reinecke <hare@suse.de>
2013-01-21 13:58:15 +0100

    Fixup Testcase 2 for new DASD layout
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- testcases/monitor_testcase_2.sh
+++ testcases/monitor_testcase_2.sh
@@ -7,8 +7,8 @@
 
 MD_NUM="md1"
 MD_NAME="testcase2"
-DEVNOS_LEFT="0.0.0200 0.0.0201 0.0.0202 0.0.0203"
-DEVNOS_RIGHT="0.0.0210 0.0.0211 0.0.0212 0.0.0213"
+DEVNOS_LEFT="0.0.0210 0.0.0211 0.0.0212 0.0.0213"
+DEVNOS_RIGHT="0.0.0220 0.0.0221 0.0.0222 0.0.0223"
 
 logger "Monitor Testcase 2: Fail both mirror sides w/o I/O"
 
@@ -19,14 +19,12 @@ activate_dasds
 clear_metadata
 
 ulimit -c unlimited
-start_md ${MD_NUM} ${MD_NAME}
+start_md ${MD_NUM}
 
 echo "Create filesystem ..."
-if ! mkfs.btrfs /dev/${MD_NUM} ; then
+if ! mkfs.ext3 /dev/${MD_NUM} ; then
     error_exit "Cannot create fs"
 fi
-# I see random errors without it ...
-sleep 1 
 
 echo "Mount filesystem ..."
 if ! mount /dev/${MD_NUM} /mnt ; then
