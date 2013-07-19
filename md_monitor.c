e687f540f11c1f82e5f248f5f251d04cf556053e
Hannes Reinecke <hare@suse.de>
2013-07-19 15:02:56 +0200

    Version 4.26
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- md_monitor.c
+++ md_monitor.c
@@ -67,7 +67,7 @@
  #endif
 #endif
 
-const char version_str[] = "md_monitor version 4.25";
+const char version_str[] = "md_monitor version 4.26";
 
 enum md_rdev_status {
 	UNKNOWN,	/* Not checked */
