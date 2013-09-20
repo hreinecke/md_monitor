6f43a13cc299a407cd8824575fc01197791879da
Hannes Reinecke <hare@suse.de>
2013-09-20 08:55:32 +0200

    monitor_testcase_3: Wait for md_monitor to pick up changes
    When reactivating the failed array udev will send out events
    for every device, which will cause 'mdadm --incremental' to be
    called for every device. This in turn will start a recovery for
    every device, so wait_for_sync will return with an error after
    the first recovery, even though there are more device events
    pending. Or another recovery might already have been started.
    So we need to wait for md_monitor to pick up changes, which
    will indicate if all devices have been processed.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- testcases/monitor_testcase_3.sh
+++ testcases/monitor_testcase_3.sh
@@ -9,6 +9,7 @@ set -o errexit
 
 MD_NUM="md1"
 MD_NAME="testcase3"
+MONITOR_TIMEOUT=30
 
 logger "Monitor Testcase 3: Disk offline/online"
 
@@ -31,6 +32,9 @@ if ! mount /dev/${MD_NUM} /mnt ; then
     error_exit "Cannot mount MD array."
 fi
 
+MD_LOG1="/tmp/monitor_${MD_NAME}_step1.log"
+md_monitor -c"ArrayStatus:/dev/${MD_NUM}" > ${MD_LOG1}
+
 echo "Write test file 1 ..."
 dd if=/dev/zero of=/mnt/testfile1 bs=4096 count=1024
 
@@ -41,10 +45,10 @@ done
 if ! mdadm --manage /dev/${MD_NUM} --fail ${DEVICES_LEFT[@]} ; then
     error_exit "Cannot fail first half in MD array $MD_NUM"
 fi
+wait_md ${MD_NUM}
 if ! mdadm --manage /dev/${MD_NUM} --remove ${DEVICES_LEFT[@]} ; then
     error_exit "Cannot fail $d in MD array $MD_NUM"
 fi
-
 wait_md ${MD_NUM}
 for devno in $DEVNOS_LEFT ; do
     if ! echo 0 > /sys/bus/ccw/devices/$devno/online ; then
@@ -62,8 +66,32 @@ for devno in $DEVNOS_LEFT ; do
 	error_exit "Cannot set device $devno online"
     fi
 done
-echo "Wait for sync"
+#
+# wait_for_sync will fail here, as the devices
+# are being activated from udev on a one-by-one
+# basis. So for every device md will start recovery
+# and wait_for_sync will exit after the currently
+# running recovery has finished, even though there
+# might be other device events pending.
+# So wait for md_monitor to pick up the changes
+# first, and then check for sync.
+#
+echo "Wait for md_monitor to pick up changes"
+sleeptime=0
+MD_LOG2="/tmp/monitor_${MD_NAME}_step2.log"
+while [ $sleeptime -lt $MONITOR_TIMEOUT ] ; do
+    md_monitor -c"ArrayStatus:/dev/${MD_NUM}" > ${MD_LOG2}
+    if diff -pu "${MD_LOG1}" "${MD_LOG2}" > /dev/null ; then
+	break;
+    fi
+    (( sleeptime ++ )) || true
+    sleep 1
+done
+if [ $sleeptime -ge $MONITOR_TIMEOUT ] ; then
+    error_exit "md_monitor did not pick up changes after $sleeptime seconds"
+fi
 if ! wait_for_sync ${MD_NUM} ; then
+    md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
     error_exit "Failed to activate first half"
 fi
 
@@ -98,7 +126,23 @@ for devno in $DEVNOS_RIGHT ; do
 	error_exit "Cannot set device $devno online"
     fi
 done
-sleep 6
+echo "Wait for md_monitor to pick up changes"
+sleeptime=0
+MD_LOG3="/tmp/monitor_${MD_NAME}_step3.log"
+while [ $sleeptime -lt $MONITOR_TIMEOUT ] ; do
+    md_monitor -c"ArrayStatus:/dev/${MD_NUM}" > ${MD_LOG3}
+    if diff -pu "${MD_LOG1}" "${MD_LOG3}" > /dev/null ; then
+	break;
+    fi
+    (( sleeptime ++ )) || true
+    sleep 1
+done
+if [ $sleeptime -ge $MONITOR_TIMEOUT ] ; then
+    error_exit "md_monitor did not pick up changes after $sleeptime seconds"
+fi
+if ! wait_for_sync ${MD_NUM} ; then
+    error_exit "Failed to activate second half"
+fi
 mdadm --detail /dev/${MD_NUM}
 ls -l /mnt
 
