5d427bd8464e30e0e2873c4bfacc82f43bcce430
Hannes Reinecke <hare@suse.de>
2013-09-20 09:28:46 +0200

    monitor_testcase_6: Cleanups
    When waiting for reservations to be registered we should be
    limiting the wait, so as not to wait forever.
    And the call to 'wget' should be put into a function for
    better readability.
    And we should always set the device to 'online' via wget
    to be sure the reserve call will succeed.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- testcases/monitor_testcase_6.sh
+++ testcases/monitor_testcase_6.sh
@@ -34,28 +34,54 @@ fi
 echo "$(date) Run I/O test"
 run_iotest /mnt;
 
+run_wget() {
+    local devno=$1
+    local action=$2
+
+    result=$(wget -nv --max-redirect=0 "http://s390vsl026.suse.de/dasd.php?ccw=${devno}&action=${action}" 2>&1)
+
+    if [ "$result" != "0 redirections exceeded." ] ; then
+	echo $result
+	return 1
+    fi
+    return 0
+}
+
 for devno in ${DEVNOS_LEFT} ; do
     echo "$(date) Reserve DASD $devno on left half ..."
-    result=$(wget -nv --max-redirect=0 "http://s390vsl026.suse.de/dasd.php?ccw=${devno}&action=Reserve" 2>&1)
-    if [ "$result" != "0 redirections exceeded." ] ; then
+    result=$(run_wget ${devno} online)
+    if [ "$result" ] ; then
+	echo "$(date) Activate DASD $devno failed; wget returned:"
+	echo "$result"
+	exit 1
+    fi
+    result=$(run_wget ${devno} Reserve)
+    if [ "$result" ] ; then
 	echo "$(date) Reserve DASD $devno failed; wget returned:"
 	echo "$result"
+	exit 1
     fi
     break;
 done
 
 echo "$(date) Wait for reservation on left half ..."
+sleeptime=0
 num=0
 while [ $num -eq 0 ] ; do
     for d in ${DASDS_LEFT[@]}; do
 	state=$(tunedasd -Q /dev/$d)
 	if [ "$state" = "other" ] ; then
-	    (( num ++ ))
+	    (( num ++ )) || true
 	    break;
 	fi
     done
+    [ $sleeptime -gt $MONITOR_TIMEOUT ] && break
     sleep 1
+    (( sleeptime ++ )) || true
 done
+if [ $num -eq 0 ] ; then
+    error_exit "$(date) no DASDs have been reserved after $sleeptime seconds"
+fi
 logger "$num DASDs reserved"
 echo "$(date) Ok. Waiting for MD to pick up changes ..."
 # Wait for md_monitor to pick up changes
@@ -66,14 +92,14 @@ while [ $num -gt 0  ] ; do
     for d in ${DASDS_LEFT[@]} ; do
 	device=$(echo $mdstat | sed -n "s/${MD_NUM}.* \(${d}1\[[0-9]*\]([F|T])\).*/\1/p")
 	if [ "$device" ] ; then
-	    (( num -- ))
+	    (( num -- )) || true
 	fi
     done
     [ $num -eq 0 ] && break
     [ $sleeptime -gt $MONITOR_TIMEOUT ] && break
     num=${#DASDS_LEFT[@]}
     sleep 1
-    (( sleeptime ++ ))
+    (( sleeptime ++ )) || true
 done
 if [ $num -eq 0 ] ; then
     echo "$(date) MD monitor picked up changes after $sleeptime seconds"
@@ -89,27 +115,34 @@ sleep $IO_TIMEOUT
 
 for devno in ${DEVNOS_LEFT} ; do
     echo "$(date) Release DASD $devno on left half ..."
-    result=$(wget -nv --max-redirect=0 "http://s390vsl026.suse.de/dasd.php?ccw=${devno}&action=Release" 2>&1 )
-    if [ "$result" != "0 redirections exceeded." ] ; then
+    result=$(run_wget ${devno} Release)
+    if [ "$result" ] ; then
 	echo "$(date) Reserve DASD $devno failed; wget returned:"
 	echo "$result"
+	exit 1
     fi
     break;
 done
 echo "$(date) Wait for reservations to be cleared ..."
+sleeptime=0
 num=${#DASDS_LEFT[@]}
 while [ $num -gt 0 ] ; do
     num=0
     for d in ${DASDS_LEFT[@]}; do
 	state=$(tunedasd -Q /dev/$d)
 	if [ "$state" = "other" ] ; then
-	    (( num ++ ))
+	    (( num ++ )) || true
 	fi
     done
     [ $num -eq 0 ] && break
+    [ $sleeptime -gt $MONITOR_TIMEOUT ] && break
     num=${#DASDS_LEFT[@]}
     sleep 1
+    (( sleeptime ++ )) || true
 done
+if [ $num -gt 0 ] ; then
+    error_exit "$(date) $num DASDs still reserved after $sleeptime seconds"
+fi
 logger "All DASDs released"
 
 echo "$(date) Ok. Waiting for MD to pick up changes ..."
@@ -120,14 +153,14 @@ while [ $num -gt 0  ] ; do
     for d in ${DASDS_LEFT[@]} ; do
 	device=$(sed -n "s/${MD_NUM}.* \(${d}1\[[0-9]*\]\).*/\1/p" /proc/mdstat)
 	if [ "$device" ] ; then
-	    (( num -- ))
+	    (( num -- )) || true
 	fi
     done
     [ $num -eq 0 ] && break
-    [ $sleeptime -gt t $MONITOR_TIMEOUT ] && break
+    [ $sleeptime -gt $MONITOR_TIMEOUT ] && break
     num=${#DASDS_LEFT[@]}
     sleep 1
-    (( sleeptime ++ ))
+    (( sleeptime ++ )) || true
 done
 if [ $num -eq 0 ] ; then
     echo "$(date) MD monitor picked up changes after $sleeptime seconds"
