73f631c51d0987b6f77727b7fcb40fdf3f8bb3e7
Hannes Reinecke <hare@suse.de>
2013-09-27 13:10:58 +0200

    monitor_testcase_5: Fix vary on/off setting
    For some reason it's not possible to have a 'configure standby'
    on linux021. As the testcase works perfectly without and disconnects
    the devices as desired we can ignore this error and continue with
    the testcase.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- testcases/monitor_testcase_5.sh
+++ testcases/monitor_testcase_5.sh
@@ -16,8 +16,6 @@ CHPID_RIGHT="0.4e"
 
 detach_other_half=1
 
-logger "Monitor Testcase 5: Chpid vary on/off"
-
 modprobe vmcp
 userid=$(vmcp q userid | cut -f 1 -d ' ')
 if [ -z "$userid" ] ; then
@@ -37,6 +35,8 @@ clear_metadata
 ulimit -c unlimited
 start_md ${MD_NUM}
 
+logger "${MD_NAME}: Chpid vary on/off"
+
 echo "$(date) Create filesystem ..."
 if ! mkfs.ext3 /dev/${MD_NUM} ; then
     error_exit "Cannot create fs"
@@ -51,20 +51,31 @@ echo "$(date) Run I/O test"
 run_iotest /mnt;
 
 # clean up for the channel path stuff
-function chp_reset() {
-    chchp -c 1 $CHPID_LEFT
-    chchp -v 1 $CHPID_LEFT
-    chchp -c 1 $CHPID_RIGHT
-    chchp -v 1 $CHPID_RIGHT
+function chp_configure() {
+    local chpid=$1
+
+    chchp -c 1 $chpid || \
+	error_exit "Cannot set chpid $chpid to 'configured'"
 }
 
-push_recovery_fn chp_reset
+function chp_vary_on() {
+    local chpid=$1
+
+    chchp -v 1 $chpid || \
+	error_exit "Cannot vary on chpid $chpid"
+}
 
 echo "$(date) vary off on chpid $CHPID_LEFT for the left side"
 logger "Vary off chpid $CHPID_LEFT"
-chchp -v 0 $CHPID_LEFT
-logger "Vary off path $CHPID_LEFT"
-chchp -c 0 $CHPID_LEFT
+chchp -v 0 $CHPID_LEFT || \
+    error_exit "Cannot vary off chpid $CHPID_LEFT"
+push_recovery_fn "chp_vary_on $CHPID_LEFT"
+logger "Set chpid $CHPID_LEFT to 'standby'"
+if chchp -c 0 $CHPID_LEFT ; then
+    push_recovery_fn "chp_configure ${CHPID_LEFT}"
+else
+    echo "Cannot set chpid $CHPID_LEFT to 'standby'"
+fi
 
 echo "$(date) Ok. Waiting for MD to pick up changes ..."
 # Wait for md_monitor to pick up changes
@@ -83,31 +94,28 @@ done
 if [ $sleeptime -lt $MONITOR_TIMEOUT ] ; then
     echo "$(date) MD monitor picked up changes after $sleeptime seconds"
 else
-    echo "$(date) ERROR: $working_disks / $raid_disks are still working"
-    # bail out after the channel path re-activation
-    TEST_FAILED=1
+    error_exit "ERROR: $working_disks / $raid_disks are still working"
 fi
 
-if [ -z "$TEST_FAILED" ] ; then
-    echo "$(date) Wait for 10 seconds"
-    sleep 10
-    mdadm --detail /dev/${MD_NUM}
-fi
+echo "$(date) Wait for 10 seconds"
+sleep 10
+mdadm --detail /dev/${MD_NUM}
 
 echo "$(date) vary on chpid $CHPID_LEFT for the left side"
-logger "Vary on path $CHPID_LEFT"
-chchp -c 1 $CHPID_LEFT
-logger "Vary on chpid $CHPID_LEFT"
-chchp -v 1 $CHPID_LEFT
+while true ; do
+    if ! pop_recovery_fn ; then
+	break;
+    fi
+done
 
 # Caveat: 'vmcp vary off' will detach the DASDs
 echo "$(date) re-attach DASDs"
 for dev in /sys/devices/css0/defunct/0.0.* ; do
+    [ -e $dev ] || continue
     devno=${dev##*/}
-    vmcp att ${devno#0.0.} \*
+    vmcp att ${devno#0.0.} \* || \
+	error_exit "Cannot attach device ${devno#0.0.}"
 done
-
-[ -n "$TEST_FAILED" ] && error_exit "TEST FAILED DUE TO THE ABOVE TIMEOUT ERROR"
 
 echo "$(date) Ok. Waiting for MD to pick up changes ..."
 # Wait for md_monitor to pick up changes
@@ -135,11 +143,6 @@ fi
 echo "$(date) MD status"
 mdadm --detail /dev/${MD_NUM}
 
-if [ -z "$detach_other_half" ] ; then
-    echo "$(date) Stop I/O test"
-    stop_iotest
-fi
-
 echo "$(date) Wait for sync"
 wait_for_sync ${MD_NUM} || \
     error_exit "Failed to synchronize array"
@@ -150,74 +153,83 @@ if ! diff -u "${START_LOG}" "${MD_LOG1}" ; then
     error_exit "current ${MD_NUM} state differs after test but should be identical to initial state"
 fi
 
-if [ -n "$detach_other_half" ] ; then
-    echo "$(date) vary off on chpid $CHPID_RIGHT for the right side"
-    logger "Vary off chpid $CHPID_RIGHT"
-    chchp -v 0 $CHPID_RIGHT
-    logger "Vary off path $CHPID_RIGHT"
-    chchp -c 0 $CHPID_RIGHT
-
-    echo "Ok. Waiting for MD to pick up changes ..."
-    sleeptime=0
-    while [ $sleeptime -lt 60  ] ; do
-	raid_status=$(sed -n 's/.*\[\([0-9]*\/[0-9]*\)\].*/\1/p' /proc/mdstat)
-	if [ "$raid_status" ] ; then
-	    raid_disks=${raid_status%/*}
-	    working_disks=${raid_status#*/}
-	    failed_disks=$(( raid_disks - working_disks))
-	    [ $working_disks -eq $failed_disks ] && break;
-	fi
-	sleep 1
-	(( sleeptime ++ ))
-    done
-    if [ $sleeptime -lt $MONITOR_TIMEOUT ] ; then
-	echo "$(date) MD monitor picked up changes after $sleeptime seconds"
-    else
-	echo "$(date) ERROR: $working_disks / $raid_disks are still working"
-	TEST_FAILED=1
-    fi
-
-    sleep 5
-    mdadm --detail /dev/${MD_NUM}
-    ls /mnt
-    echo "$(date) vary on chpid $CHPID_RIGHT for the right side"
-    logger "Vary on path $CHPID_RIGHT"
-    chchp -c 1 $CHPID_RIGHT
-    logger "Vary on chpid $CHPID_RIGHT"
-    chchp -v 1 $CHPID_RIGHT
-    [ -n "$TEST_FAILED" ] && error_exit "TEST FAILED DUE TO THE ABOVE TIMEOUT ERROR"
-
-    echo "$(date) Ok. Waiting for MD to pick up changes ..."
-    # Wait for md_monitor to pick up changes
-    sleeptime=0
-    num=${#DASDS_RIGHT[@]}
-    while [ $num -gt 0  ] ; do
-	[ $sleeptime -ge $MONITOR_TIMEOUT ] && break
-	for d in ${DASDS_RIGHT[@]} ; do
-	    device=$(sed -n "s/${MD_NUM}.* \(${d}1\[[0-9]*\]\).*/\1/p" /proc/mdstat)
-	    if [ "$device" ] ; then
-		(( num -- ))
-	    fi
-	done
-	[ $num -eq 0 ] && break
-	num=${#DASDS_RIGHT[@]}
-	sleep 1
-	(( sleeptime ++ ))
-    done
-    if [ $sleeptime -lt $MONITOR_TIMEOUT ] ; then
-	echo "$(date) MD monitor picked up changes after $sleeptime seconds"
-    else
-	error_exit "$(date) ERROR: $num devices are still faulty"
-    fi
-    
-    echo "$(date) Stop I/O test"
-    stop_iotest
-
-    wait_for_sync ${MD_NUM} || \
-	error_exit "Failed to synchronize array"
-
-    mdadm --detail /dev/${MD_NUM}
+echo "$(date) vary off on chpid $CHPID_RIGHT for the right side"
+logger "Vary off chpid $CHPID_RIGHT"
+chchp -v 0 $CHPID_RIGHT || \
+    error_exit "Cannot vary off chpid $CHPID_RIGHT"
+push_recovery_fn "chp_vary_on $CHPID_RIGHT"
+logger "Set chpid $CHPID_RIGHT to 'standby'"
+if chchp -c 0 $CHPID_RIGHT ; then
+    push_recovery_fn "chp_configure ${CHPID_RIGHT}"
+else
+    echo "Cannot set chpid $CHPID_RIGHT to 'standby'"
 fi
+
+echo "Ok. Waiting for MD to pick up changes ..."
+sleeptime=0
+while [ $sleeptime -lt 60  ] ; do
+    raid_status=$(sed -n 's/.*\[\([0-9]*\/[0-9]*\)\].*/\1/p' /proc/mdstat)
+    if [ "$raid_status" ] ; then
+	raid_disks=${raid_status%/*}
+	working_disks=${raid_status#*/}
+	failed_disks=$(( raid_disks - working_disks))
+	[ $working_disks -eq $failed_disks ] && break;
+    fi
+    sleep 1
+    (( sleeptime ++ ))
+done
+if [ $sleeptime -lt $MONITOR_TIMEOUT ] ; then
+    echo "$(date) MD monitor picked up changes after $sleeptime seconds"
+else
+    error_exit "ERROR: $working_disks / $raid_disks are still working"
+fi
+sleep 5
+mdadm --detail /dev/${MD_NUM}
+ls /mnt
+echo "$(date) vary on chpid $CHPID_RIGHT for the right side"
+logger "Vary on path $CHPID_RIGHT"
+while true ; do
+    if ! pop_recovery_fn ; then
+	break;
+    fi
+done
+
+echo "$(date) Ok. Waiting for MD to pick up changes ..."
+    # Wait for md_monitor to pick up changes
+sleeptime=0
+num=${#DASDS_RIGHT[@]}
+while [ $num -gt 0  ] ; do
+    [ $sleeptime -ge $MONITOR_TIMEOUT ] && break
+    for d in ${DASDS_RIGHT[@]} ; do
+	device=$(sed -n "s/${MD_NUM}.* \(${d}1\[[0-9]*\]\).*/\1/p" /proc/mdstat)
+	if [ "$device" ] ; then
+	    (( num -- ))
+	fi
+    done
+    [ $num -eq 0 ] && break
+    num=${#DASDS_RIGHT[@]}
+    sleep 1
+    (( sleeptime ++ ))
+done
+if [ $sleeptime -lt $MONITOR_TIMEOUT ] ; then
+    echo "$(date) MD monitor picked up changes after $sleeptime seconds"
+else
+    error_exit "$(date) ERROR: $num devices are still faulty"
+fi
+    
+echo "$(date) Stop I/O test"
+stop_iotest
+
+wait_for_sync ${MD_NUM} || \
+    error_exit "Failed to synchronize array"
+
+MD_LOG2="/tmp/monitor_${MD_NAME}_step2.log"
+mdadm --detail /dev/${MD_NUM} | sed '/Update Time/D;/Events/D' | tee ${MD_LOG2}
+if ! diff -u "${START_LOG}" "${MD_LOG2}" ; then
+    error_exit "current ${MD_NUM} state differs after test but should be identical to initial state"
+fi
+
+logger "${MD_NAME}: success"
 
 echo "$(date) Umount filesystem ..."
 umount /mnt
