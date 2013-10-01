8303d4809ac56062d38f220b984ba465c1e3440c
Hannes Reinecke <hare@suse.de>
2013-10-01 07:54:56 +0200

    Version 5.0
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- md_monitor.c
+++ md_monitor.c
@@ -57,7 +57,7 @@
 #include "md_debug.h"
 #include "dasd_ioctl.h"
 
-const char version_str[] = "md_monitor version 4.26";
+const char version_str[] = "md_monitor version 5.0";
 
 enum md_rdev_status {
 	UNKNOWN,	/* Not checked */
