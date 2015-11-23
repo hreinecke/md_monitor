12268488a067e08874f79c053b6c5a7fe9b4c553
Hannes Reinecke <hare@suse.de>
2015-11-23 11:18:03 +0100

    monitor_testcase12: Use device remove/rescan for zFCP
    For zFCP we should be detaching and rescanning the SCSI devices.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- testcases/monitor_testcase_12.sh
+++ testcases/monitor_testcase_12.sh
@@ -25,6 +25,13 @@ function attach_dasd() {
     fi
 }
 
+function rescan_shost() {
+    local shost=$1
+
+    echo '- - -' > /sys/class/scsi_host/${shost}/scan || \
+	error_exit "Failed to rescan host ${shost}"
+}
+
 stop_md $MD_NUM
 
 activate_devices
@@ -55,28 +62,60 @@ echo "$(date) Run I/O test"
 run_iotest /mnt;
 
 MD_LOG1="/tmp/monitor_${MD_NAME}_step1.log"
-md_monitor -c"ArrayStatus:/dev/${MD_NUM}" | tee ${MD_LOG1}
+md_monitor -c"ArrayStatus:/dev/${MD_NUM}" | sed -n 's/\(.*\) slot [0-9]*\/[0-9]* \(.*\)/\1 \2/p' | tee ${MD_LOG1}
 
-for devno in ${DEVNOS_LEFT} ; do
-    echo "$(date) Waiting for $SLEEPTIME seconds ..."
-    sleep $SLEEPTIME
+if [ -n "$DEVNOS_LEFT" ] ; then
+    for devno in ${DEVNOS_LEFT} ; do
+	echo "$(date) Waiting for $SLEEPTIME seconds ..."
+	sleep $SLEEPTIME
 
-    echo "$(date) Detach left device $devno ..."
-    dasd=${devno##*.}
-    if ! vmcp det ${dasd} ; then
-	error_exit "Cannot detach DASD ${dasd}"
-    fi
-    push_recovery_fn "attach_dasd $userid ${dasd}"
-    echo "$(date) Waiting for 15 seconds ..."
-    sleep 15
-    md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
-    echo "$(date) Attach left device $devno ..."
-    pop_recovery_fn || \
-	error_exit "Cannot attach DASD ${dasd}"
+	echo "$(date) Detach left device $devno ..."
+	dasd=${devno##*.}
+	if ! vmcp det ${dasd} ; then
+	    error_exit "Cannot detach DASD ${dasd}"
+	fi
+	push_recovery_fn "attach_dasd $userid ${dasd}"
 
-    md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
-done
+	echo "$(date) Waiting for 15 seconds ..."
+	sleep 15
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+	echo "$(date) Attach left device $devno ..."
+	pop_recovery_fn || \
+	    error_exit "Cannot attach DASD ${dasd}"
 
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+    done
+else
+    for wwid in ${SCSIID_LEFT} ; do
+	echo "$(date) Waiting for $SLEEPTIME seconds ..."
+	sleep $SLEEPTIME
+
+	echo "$(date) Remove left device $wwid ..."
+	paths=$(multipathd -k"show map $wwid topology" | \
+	        sed -n 's/.*[0-9]*:[0-9]*:[0-9]*:[0-9]* \(sd[a-z]*\) .*/\1/p')
+	for path in ${paths} ; do
+	    echo 1 > /sys/block/$path/device/delete || \
+		error_exit "Cannot remove device ${path} from ${wwid}"
+	done
+	recovery_fn=0
+	for shost in ${SHOSTS_LEFT[@]} ; do
+	    push_recovery_fn "rescan_shost $shost"
+	    (( recovery_fn++ )) || true
+	done
+
+	echo "$(date) Waiting for 15 seconds ..."
+	sleep 15
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+	echo "$(date) Attach left device $wwid ..."
+	while [ $recovery_fn -gt 0 ] ; do
+	    pop_recovery_fn || \
+		error_exit "Cannot rescan devices for ${wwid}"
+	    (( recovery_fn-- )) || true
+	done
+
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+    done
+fi
 echo "$(date) Wait for sync"
 if ! wait_for_sync ${MD_NUM} ; then
     md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
@@ -84,32 +123,65 @@ if ! wait_for_sync ${MD_NUM} ; then
     error_exit "Failed to synchronize array"
 fi
 echo "$(date) mirror synchronized"
+check_md_log
 MD_LOG2="/tmp/monitor_${MD_NAME}_step2.log"
-md_monitor -c"ArrayStatus:/dev/${MD_NUM}" | tee ${MD_LOG2}
+md_monitor -c"ArrayStatus:/dev/${MD_NUM}" | sed -n 's/\(.*\) slot [0-9]*\/[0-9]* \(.*\)/\1 \2/p' | tee ${MD_LOG2}
 if ! diff -pu ${MD_LOG1} ${MD_LOG2} ; then
     stop_iotest
     error_exit "inconsistent md_monitor status after detaching left half"
 fi
 
-for devno in ${DEVNOS_RIGHT} ; do
-    echo "$(date) Waiting for $SLEEPTIME seconds ..."
-    sleep $SLEEPTIME
+if [ -n "$DEVNOS_RIGHT" ] ; then
+    for devno in ${DEVNOS_RIGHT} ; do
+	echo "$(date) Waiting for $SLEEPTIME seconds ..."
+	sleep $SLEEPTIME
     
-    echo "$(date) Detach right device $devno ..."
-    dasd=${devno##*.}
-    if ! vmcp det ${dasd} ; then
-	error_exit "Cannot detach DASD ${dasd}"
-    fi
-    push_recovery_fn "attach_dasd $userid ${dasd}"
-    echo "$(date) Waiting for 15 seconds ..."
-    sleep 15
-    md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
-    echo "$(date) Attach right device $devno ..."
-    if ! pop_recovery_fn ; then
-	error_exit "Cannot attach DASD ${dasd}"
-    fi
-    md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
-done
+	echo "$(date) Detach right device $devno ..."
+	dasd=${devno##*.}
+	if ! vmcp det ${dasd} ; then
+	    error_exit "Cannot detach DASD ${dasd}"
+	fi
+	push_recovery_fn "attach_dasd $userid ${dasd}"
+	echo "$(date) Waiting for 15 seconds ..."
+	sleep 15
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+	echo "$(date) Attach right device $devno ..."
+	if ! pop_recovery_fn ; then
+	    error_exit "Cannot attach DASD ${dasd}"
+	fi
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+    done
+else
+    for wwid in ${SCSIID_RIGHT} ; do
+	echo "$(date) Waiting for $SLEEPTIME seconds ..."
+	sleep $SLEEPTIME
+
+	echo "$(date) Remove right device $wwid ..."
+	paths=$(multipathd -k"show map $wwid topology" | \
+	        sed -n 's/.*[0-9]*:[0-9]*:[0-9]*:[0-9]* \(sd[a-z]*\) .*/\1/p')
+	for path in ${paths} ; do
+	    echo 1 > /sys/block/$path/device/delete || \
+		error_exit "Cannot remove device ${path} from ${wwid}"
+	done
+	recovery_fn=0
+	for shost in ${SHOSTS_RIGHT[@]} ; do
+	    push_recovery_fn "rescan_shost $shost"
+	    (( recovery_fn++ )) || true
+	done
+
+	echo "$(date) Waiting for 15 seconds ..."
+	sleep 15
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+	echo "$(date) Attach right device $wwid ..."
+	while [ $recovery_fn -gt 0 ] ; do
+	    pop_recovery_fn || \
+		error_exit "Cannot rescan devices for ${wwid}"
+	    (( recovery_fn-- )) || true
+	done
+
+	md_monitor -c"ArrayStatus:/dev/${MD_NUM}"
+    done
+fi
 
 echo "$(date) Wait for sync"
 if ! wait_for_sync ${MD_NUM} ; then
@@ -119,7 +191,7 @@ if ! wait_for_sync ${MD_NUM} ; then
 fi
 echo "$(date) sync finished"
 MD_LOG3="/tmp/monitor_${MD_NAME}_step3.log"
-md_monitor -c"ArrayStatus:/dev/${MD_NUM}" | tee ${MD_LOG3}
+md_monitor -c"ArrayStatus:/dev/${MD_NUM}" | sed -n 's/\(.*\) slot [0-9]*\/[0-9]* \(.*\)/\1 \2/p' | tee ${MD_LOG3}
 if ! diff -pu ${MD_LOG1} ${MD_LOG3} ; then
     stop_iotest
     error_exit "inconsistent md monitor status after detaching right half"
