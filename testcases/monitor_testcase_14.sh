a66fb3205a3b8d9dff3052b778902bf5b47071ec
Hannes Reinecke <hare@suse.de>
2015-11-23 13:38:26 +0100

    monitor_testcase14: Update for zFCP
    Use 'online'/'running' to simulate quiesce.
    Real SCSI quiesce doesn't start the command timer, so we'll never
    get an I/O completion back.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- testcases/monitor_testcase_14.sh
+++ testcases/monitor_testcase_14.sh
@@ -11,6 +11,21 @@ MD_NUM="md1"
 MD_NAME="testcase14"
 MONITOR_TIMEOUT=60
 
+function resume_dasd() {
+    local dasd=$1
+
+    setdasd -q 0 -d /dev/${dasd} || \
+	error_exit "Cannot resume /dev/${dasd}"
+}
+
+function online_scsi() {
+    local sdev=$1
+
+    if ! echo running > /sys/block/$sdev/device/state ; then
+	error_exit "Cannot set device $sdev online"
+    fi
+}
+
 logger "Monitor Testcase 14: Disk quiesce/resume"
 
 stop_md $MD_NUM
@@ -35,11 +50,20 @@ fi
 echo "$(date) Write test file 1 ..."
 dd if=/dev/zero of=/mnt/testfile1 bs=4096 count=1024
 
-echo "$(date) Quiesce disks on first half ..."
-for d in ${DEVICES_LEFT[@]} ; do
-    setdasd -q 1 -d /dev/${d} || \
-	error_exit "Cannot quiesce /dev/${d}"
-done
+if [ -n "$DEVNOS_LEFT" ] ; then
+    echo "$(date) Quiesce disks on first half ..."
+    for d in ${DEVICES_LEFT[@]} ; do
+	setdasd -q 1 -d /dev/${d} || \
+	    error_exit "Cannot quiesce /dev/${d}"
+	push_recovery_fn "resume_dasd ${d}"
+    done
+else
+    for sdev in ${SDEVS_LEFT[@]} ; do
+	echo offline > /sys/block/$sdev/device/state || \
+	    error_exit "Cannot offline device $sdev"
+	push_recovery_fn "online_scsi $sdev"
+    done
+fi
 
 wait_for_md_failed $MONITOR_TIMEOUT
 
@@ -51,10 +75,21 @@ echo "$(date) Wait for 10 seconds"
 sleep 10
 
 echo "$(date) Quiesce disks on second half ..."
-for d in ${DEVICES_RIGHT[@]} ; do
-    setdasd -q 1 -d /dev/${d} || \
-	error_exit "Cannot quiesce /dev/${d}"
-done
+
+if [ -n "$DEVNOS_RIGHT" ] ; then
+    for d in ${DEVICES_RIGHT[@]} ; do
+	setdasd -q 1 -d /dev/${d} || \
+	    error_exit "Cannot quiesce /dev/${d}"
+	push_recovery_fn "resume_dasd ${d}"
+    done
+else
+    for sdev in ${SDEVS_RIGHT[@]} ; do
+	echo offline > /sys/block/$sdev/device/state || \
+	    error_exit "Cannot offline device $sdev"
+	push_recovery_fn "online_scsi $sdev"
+    done
+fi
+
 
 # Needs to be started in the background, as it'll hang otherwise
 ( echo "Write test file 3 ..."; \
@@ -67,10 +102,20 @@ sleep 10
 md_monitor -c "MonitorStatus:/dev/${MD_NUM}"
 
 echo "$(date) Resume disks on second half ..."
-for d in ${DEVICES_RIGHT[@]} ; do
-    setdasd -q 0 -d /dev/${d} || \
-	error_exit "Cannot resume /dev/${d}"
-done
+
+if [ -n "$DEVNOS_RIGHT" ] ; then
+    for d in ${DEVICES_RIGHT[@]} ; do
+	if ! pop_recovery_fn ; then
+	    break;
+	fi
+    done
+else
+    for sdev in ${SDEVS_RIGHT[@]} ; do
+	if ! pop_recovery_fn ; then
+	    break;
+	fi
+    done
+fi
 
 md_monitor -c "MonitorStatus:/dev/${MD_NUM}"
 
@@ -78,10 +123,19 @@ echo "Write test file 4 ..."
 dd if=/dev/zero of=/mnt/testfile4 bs=4096 count=1024
 
 echo "$(date) Resume disks on first half ..."
-for d in ${DEVICES_LEFT[@]} ; do
-    setdasd -q 0 -d ${d} || \
-	error_exit "Cannot resume /dev/${d}"
-done
+if [ -n "$DEVNOS_LEFT" ] ; then
+    for d in ${DEVICES_LEFT[@]} ; do
+	if ! pop_recovery_fn ; then
+	    break;
+	fi
+    done
+else
+    for sdev in ${SDEVS_LEFT[@]} ; do
+	if ! pop_recovery_fn ; then
+	    break;
+	fi
+    done
+fi
 
 wait_for_md_running_left $MONITOR_TIMEOUT
 
