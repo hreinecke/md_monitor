9048facadad5637f289594047907645ea83b2321
Martin Wilck <mwilck@suse.com>
2018-11-24 00:01:04 +0100

    fail_md: fix pending_side bit op
    md_dev->pending_side and md_dev->degraded are both set
    from (1 << side). Shifting again breaks the bitop.
    
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- md_monitor.c
+++ md_monitor.c
@@ -1762,7 +1762,7 @@ static int fail_md(struct md_monitor *md_dev)
 		warn("%s: no pending side", md_name);
 		return 0;
 	}
-	if (md_dev->degraded & (1 << md_dev->pending_side)) {
+	if (md_dev->degraded & md_dev->pending_side) {
 		info("%s: mirror side %d already failed", md_name,
 		     (md_dev->pending_side >> 1));
 		pthread_mutex_unlock(&md_dev->status_lock);
