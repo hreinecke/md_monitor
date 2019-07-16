hreinecke/md_monitor

commit 2379a9c48320bd9149159fef8107301d7a97a953

md_monitor: fixup crash in display_io_status
display_io_status may crash if md_dev->raid_disks isn't set
properly.

md_monitor.c
@@ -2028,16 +2028,11 @@ static int display_md_status(struct md_monitor *md_dev, char *buf, int buflen)
 static int display_io_status(struct md_monitor *md_dev, char *buf, int buflen)
 {
 	struct device_monitor *dev;
-	int slot, max_slot = md_dev->raid_disks;
+	int slot, max_slot = -1;
 	int len = 0;
 	char status;
 
-	if (max_slot >= buflen) {
-		warn("%s: CLI buffer too small, min %d",
-		     md_dev->dev_name, max_slot);
-		max_slot = buflen;
-	}
-	memset(buf, '.', max_slot);
+	memset(buf, '.', buflen - 1);
 
 	pthread_mutex_lock(&md_dev->device_lock);
 	list_for_each_entry(dev, &md_dev->children, siblings) {
@@ -2045,6 +2040,8 @@ static int display_io_status(struct md_monitor *md_dev, char *buf, int buflen)
 		if (slot < 0)
 			continue;
 		if (slot >= max_slot)
+			max_slot = slot;
+		if (slot >= buflen)
 			continue;
 		pthread_mutex_lock(&dev->lock);
 		while (dev->ioctx && dev->io_status == IO_UNKNOWN)
@@ -2058,6 +2055,14 @@ static int display_io_status(struct md_monitor *md_dev, char *buf, int buflen)
 			len = slot + 1;
 	}
 	pthread_mutex_unlock(&md_dev->device_lock);
+
+	max_slot++;
+	if (max_slot >= buflen) {
+		warn("%s: CLI buffer too small, min %d",
+		     md_dev->dev_name, max_slot);
+		max_slot = buflen - 1;
+	}
+	buf[max_slot] = '\0';
 
 	info("%s: io status %s", md_dev->dev_name, buf);
 	return max_slot;

commit 4176250f66795a1363ed2fbfbc030f366b8f40d3

md_monitor: display_md_status: don't omit trailing dots
display_md_status() and display_io_status() end
output at the index of the last child of the md array they encounter
while walking the sibling list. This may lead to missing trailing dots (".")
if a disk at the end of the array was in removed.

This behavior was introduced by the past commits mentioned below,
with the intention to fix a crash that occured when a degraded
md_monitor struct (with md_dev->raid_disks == -1) was encountered.
Fix this case by using md_dev->raid_disks again if it is larger
than the last slot encountered.

Fixes: 2379a9c md_monitor: fixup crash in display_io_status
Dixes: 62d385b md_monitor: fixup crash in display_md_status

md_monitor.c
@@ -2026,6 +2026,8 @@ static int display_md_status(struct md_monitor *md_dev, char *buf, int buflen)
 	}
 	pthread_mutex_unlock(&md_dev->device_lock);
 	max_slot++;
+	if (md_dev->raid_disks < buflen && md_dev->raid_disks > max_slot)
+		max_slot = md_dev->raid_disks;
 	if (max_slot >= buflen) {
 		warn("%s: CLI buffer too small, min %d\n",
 		     md_dev->dev_name, max_slot);
@@ -2068,6 +2070,8 @@ static int display_io_status(struct md_monitor *md_dev, char *buf, int buflen)
 	pthread_mutex_unlock(&md_dev->device_lock);
 
 	max_slot++;
+	if (md_dev->raid_disks < buflen && md_dev->raid_disks > max_slot)
+		max_slot = md_dev->raid_disks;
 	if (max_slot >= buflen) {
 		warn("%s: CLI buffer too small, min %d",
 		     md_dev->dev_name, max_slot);