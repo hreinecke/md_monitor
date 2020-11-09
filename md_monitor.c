hreinecke/md_monitor

commit bfc8e1b78c4e2177bd9ce0316fb7cda904e66e5b

lookup_md_component: take device lock
dev->md_name is not static. We need to take the lock when
referencing it.

Signed-off-by: Martin Wilck <mwilck@suse.com>

md_monitor.c
@@ -314,6 +314,7 @@ static struct device_monitor * lookup_md_component(struct md_monitor *md_dev,
 	pthread_mutex_unlock(&md_dev->status_lock);
 	pthread_mutex_lock(&md_dev->device_lock);
 	list_for_each_entry(tmp, &md_dev->children, siblings) {
+		/* No locking required, tmp->device is static */
 		if (lookup_symlinks) {
 			struct udev_list_entry *entry;
 			const char *tmpname, *ptr;
@@ -334,14 +335,17 @@ static struct device_monitor * lookup_md_component(struct md_monitor *md_dev,
 				}
 			}
 		}
+		pthread_mutex_lock(&tmp->lock);
 		if (!strncmp(devname, tmp->md_name,
 			     strlen(devname))) {
 			found = tmp;
+			pthread_mutex_unlock(&tmp->lock);
 			break;
 		}
 		if (!strncmp(devname, tmp->dev_name,
 			     strlen(devname))) {
 			found = tmp;
+			pthread_mutex_unlock(&tmp->lock);
 			break;
 		}
 	}

commit 1bb0d1161714b6c4b159c143671a5b476ef37f7e

md_monitor: Fix locking of dev->lock in lookup_md_component()
Add a missing call to pthread_mutex_unlock(&tmp->lock) in
lookup_md_component() when an input devname does not match a name of the
currently processed device. The lock was wrongly released only when the
devname matched.

Fixes: bfc8e1b ("lookup_md_component: take device lock")

md_monitor.c
@@ -347,6 +347,7 @@ static struct device_monitor * lookup_md_component(struct md_monitor *md_dev,
 			pthread_mutex_unlock(&tmp->lock);
 			break;
 		}
+		pthread_mutex_unlock(&tmp->lock);
 	}
 out:
 	pthread_mutex_unlock(&md_dev->device_lock);