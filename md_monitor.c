hreinecke/md_monitor

commit 22922c4239f7850a871b7d62c98becad90076b45

md_monitor: remove the obsolete md_dev from md_list
There is a race condition between md device was shutting down and
md_monitor was started. md_monitor() function might failed during this
process, thus an obsolete md_dev was linked to md_list. Even though the
md device was reassembled, the attributes generated from CHANGE event
can't be obtained. So md_monitor would report the md device doesn't
exist when searching with md_name.

References: bsc#1193465
Signed-off-by: Lidong Zhong <lidong.zhong@suse.com>

md_monitor.c
@@ -2029,10 +2029,15 @@ static void discover_md(struct udev *udev)
 		md_dev = udev_device_new_from_syspath(udev, md_devpath);
 		warn("Testing %s", md_devpath);
 		if (md_dev) {
-			monitor_md(md_dev);
+			if (monitor_md(md_dev) != 0) {
+				md_devpath = udev_device_get_sysname(md_dev);
+				unmonitor_md(md_devpath);
+				goto unref;
+            }
 			udev_device_unref(md_dev);
 		}
 	}
+unref:
 	udev_enumerate_unref(md_enumerate);
 }
 

commit f5a4492a84e4c0aed909125bd4e952b100a7e7d3

monitor: md_monitor skips the other md devices when an obsolete one is discovered
We tried to remove an obsolete md device in the following patch
commit 22922c4239f7850a871b7d62c98becad90076b45
Author: Lidong Zhong <lidong.zhong@suse.com>
Date:   Tue Dec 28 15:29:29 2021 +0800

    md_monitor: remove the obsolete md_dev from md_list

but it will skip the other md devices to monitor.

References: bsc#1193465
Fixes: 22922c4239f7 (md_monitor: remove the obsolete md_dev from md_list)

md_monitor.c
@@ -2031,12 +2031,12 @@ static void discover_md(struct udev *udev)
 			if (monitor_md(md_dev) != 0) {
 				md_devpath = udev_device_get_sysname(md_dev);
 				unmonitor_md(md_devpath);
-				goto unref;
+				/* udev_device_unref() is already called in unmonitor_md() */
+				continue;
             }
 			udev_device_unref(md_dev);
 		}
 	}
-unref:
 	udev_enumerate_unref(md_enumerate);
 }
 